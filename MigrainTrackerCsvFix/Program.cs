fca8d69ad0e93e98d91004fd5f0f1b03dffa6a94
Kyle <Kyle.Altemara@gmail.com>
2024-09-05 14:59:04 -0700

    Program now read in args
    Can now drop a CSV file or a folder with CSV files on the executable and it will try to process all CSV files it was given

--- MigrainTrackerCsvFix/Program.cs
+++ MigrainTrackerCsvFix/Program.cs
@@ -10,84 +10,118 @@ using System.Linq;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        string filePath = @"C:\Sandbox\JoshMigraineCsvFixer\SymptomLog.csv";
-        var symptomLogs = ReadSymptomLogs(filePath);
-        var groupedLogs = new Dictionary<(string date, string sympton), NewSymptomLog>();
-
-        foreach (var log in symptomLogs)
+        foreach (var arg in args)
         {
-            var symptom = log.Detail?.Replace("(Mild)", string.Empty)
-                                     .Replace("(Moderate)", string.Empty)
-                                     .Replace("(Severe)", string.Empty)
-                                     .Replace("(Unbearable)", string.Empty)
-                                     .Replace("(when, duration, where, 1-10)", string.Empty)
-                                     .Replace("(time, duration, 1-10)", string.Empty)
-                                     .Trim() ?? "";
-            if (log.Category == "Extra notes")
+            if (File.Exists(arg) && Path.GetExtension(arg) == ".csv")
             {
-                symptom = "Extra notes";
+                FixMigraineCsv(arg);
             }
-
-            var key = (log.Date!, symptom);
-            if (groupedLogs.TryGetValue(key, out var newlog))
+            else if (Directory.Exists(arg))
             {
-                switch (log.TimeOfDay)
+                var files = Directory.GetFiles(arg, "*.csv");
+                foreach (var file in files)
                 {
-                    case "AM":
-                    case "am":
-                        newlog.AmLevel = log.RatingAmount;
-                        break;
-                    case "Mid":
-                    case "mid":
-                        newlog.MidLevel = log.RatingAmount;
-                        break;
-                    case "PM":
-                    case "pm":
-                        newlog.PmLevel = log.RatingAmount;
-                        break;
-                    default:
-                        break;
-                }
-
-                if (!string.IsNullOrEmpty(log.Notes))
-                {
-                    if (string.IsNullOrEmpty(newlog.Notes))
-                    {
-                        newlog.Notes = log.Notes;
-                    }
-                    else
-                    {
-                        newlog.Notes += Environment.NewLine + log.Notes;
-                    }
+                    FixMigraineCsv(file);
                 }
             }
-            else
+        }
+
+        Console.WriteLine("Press any key to exit . . .");
+        Console.ReadKey();
+    }
+
+    private static void FixMigraineCsv(string filePath)
+    {
+        Console.Write($"Processing file: {filePath} . . .");
+        try
+        {
+            var symptomLogs = ReadSymptomLogs(filePath);
+            var groupedLogs = new Dictionary<(string date, string sympton), NewSymptomLog>();
+
+            foreach (var log in symptomLogs)
             {
-                var newLog = new NewSymptomLog
+                var symptom = log.Detail?.Replace("(Mild)", string.Empty)
+                                         .Replace("(Moderate)", string.Empty)
+                                         .Replace("(Severe)", string.Empty)
+                                         .Replace("(Unbearable)", string.Empty)
+                                         .Replace("(when, duration, where, 1-10)", string.Empty)
+                                         .Replace("(time, duration, 1-10)", string.Empty)
+                                         .Trim() ?? "";
+                if (log.Category == "Extra notes")
                 {
-                    Date = log.Date,
-                    DateFormattedString = log.DateFormattedString,
-                    Weekday = log.Weekday,
-                    Symptom = symptom,
-                    AmLevel = log.TimeOfDay.Equals("am", StringComparison.CurrentCultureIgnoreCase) ? log.RatingAmount : null,
-                    MidLevel = log.TimeOfDay.Equals("mid", StringComparison.CurrentCultureIgnoreCase) ? log.RatingAmount : null,
-                    PmLevel = log.TimeOfDay.Equals("pm", StringComparison.CurrentCultureIgnoreCase) ? log.RatingAmount : null,
-                    Notes = log.Notes,
-                };
-                groupedLogs.Add(key, newLog);
+                    symptom = "Extra notes";
+                }
+
+                var key = (log.Date!, symptom);
+                if (groupedLogs.TryGetValue(key, out var newlog))
+                {
+                    switch (log.TimeOfDay)
+                    {
+                        case "AM":
+                        case "am":
+                            newlog.AmLevel = log.RatingAmount;
+                            break;
+                        case "Mid":
+                        case "mid":
+                            newlog.MidLevel = log.RatingAmount;
+                            break;
+                        case "PM":
+                        case "pm":
+                            newlog.PmLevel = log.RatingAmount;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (!string.IsNullOrEmpty(log.Notes))
+                    {
+                        if (string.IsNullOrEmpty(newlog.Notes))
+                        {
+                            newlog.Notes = log.Notes;
+                        }
+                        else
+                        {
+                            newlog.Notes += Environment.NewLine + log.Notes;
+                        }
+                    }
+                }
+                else
+                {
+                    var newLog = new NewSymptomLog
+                    {
+                        Date = log.Date,
+                        DateFormattedString = log.DateFormattedString,
+                        Weekday = log.Weekday,
+                        Symptom = symptom,
+                        AmLevel = log.TimeOfDay!.Equals("am", StringComparison.CurrentCultureIgnoreCase) ? log.RatingAmount : null,
+                        MidLevel = log.TimeOfDay!.Equals("mid", StringComparison.CurrentCultureIgnoreCase) ? log.RatingAmount : null,
+                        PmLevel = log.TimeOfDay!.Equals("pm", StringComparison.CurrentCultureIgnoreCase) ? log.RatingAmount : null,
+                        Notes = log.Notes,
+                    };
+                    groupedLogs.Add(key, newLog);
+                }
+
             }
 
-        }
+            foreach (var group in groupedLogs)
+            {
+                group.Value.FormatNotes(100);
+            }
 
-        foreach (var group in groupedLogs)
+            WriteNewSymptomLogs(Path.Combine(filePath, "..", "updated_" + Path.GetFileName(filePath)), groupedLogs.Values);
+            Console.WriteLine("Succeded");
+            Console.WriteLine($"CSV file has been processed and saved as updated_{Path.GetFileName(filePath)}");
+            Console.WriteLine();
+        }
+        catch (Exception ex)
         {
-            group.Value.FormatNotes(100);
-            Console.WriteLine(group.Value);
+            Console.WriteLine("Failed");
+            Console.WriteLine("CSV file could not be processed.");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
         }
-
-        WriteGroupedLogsToCsv(@"C:\Sandbox\JoshMigraineCsvFixer\UpdatedSymptomLog.csv", groupedLogs.Values);
     }
 
     static List<SymptomLog> ReadSymptomLogs(string filePath)
@@ -107,7 +141,7 @@ class Program
         return records;
     }
 
-    static void WriteGroupedLogsToCsv(string filePath, IEnumerable<NewSymptomLog> logs)
+    static void WriteNewSymptomLogs(string filePath, IEnumerable<NewSymptomLog> logs)
     {
         var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
         {
